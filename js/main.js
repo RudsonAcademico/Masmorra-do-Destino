e676c568d7e4efbe6d2dec109d820d6edcc17794
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2025-02-17 17:13:27 +0000

    pode lançar ja

--- js/main.js
+++ js/main.js
@@ -12,10 +12,14 @@ function atribuir(){
     sorte = document.getElementById("sorte").value;
     sorte = parseInt(sorte);
     if (forca + defesa + sorte > 10){
-        window.alert("atributos com pontuação superior a permitida");
+        window.alert("Atributos com pontuação superior a permitida");
     } else if (forca + defesa + sorte < 10){
-        window.alert("atributos com pontuação abaixo da permitida");
-    } else {
+        window.alert("Atributos com pontuação abaixo da permitida");
+    } else if (forca < 0 || defesa < 0 || sorte < 0 || !nome || forca == NaN || defesa == NaN || sorte == NaN){
+        window.alert("Valores Invalidos");
+    }
+    
+    else {
         window.location.href = `/pages/masmorra.html?nome=${nome}&forca=${forca}&defesa=${defesa}&sorte=${sorte}`;
     }
 }
